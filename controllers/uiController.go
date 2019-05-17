68c19282745887a2a4fbd2a1dc3c438fcffa1673
dukler <iarwain@hotmail.com.ar>
2019-05-17 18:16:51 -0300

    re-read json bucket

--- controllers/uiController.go
+++ controllers/uiController.go
@@ -17,14 +17,15 @@ var UIEndpoint = func (w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	var screen string
 	screen = vars["Screen"]
-	data := getScreenJson(screen)
-	nUI := ui.NormalizeData(data)
+
 
 	switch req.Method {
 		case "GET":
 			w.Header().Set("Content-Type", "application/json")
 			w.Header().Add("Cache-Control", "max-age=86400")
 			w.WriteHeader(http.StatusOK)
+			data := getScreenJson(screen)
+			nUI := ui.NormalizeData(data)
 			response, err := json.Marshal(nUI)
 			w.Write(response)
 			if err != nil {
