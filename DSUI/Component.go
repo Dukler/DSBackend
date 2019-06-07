d1ba60447632502c68d28fc794af4a9994a408e5
dukler <iarwain@hotmail.com.ar>
2019-06-07 02:02:19 -0300

    wtf

--- DSUI/Component.go
+++ DSUI/Component.go
@@ -6,14 +6,10 @@ type DSComponent struct {
 	Actions        	DSActions 		`json:"actions"`
 	Wrapper
 	Value         	*interface{} 	`json:"value"`
-	ValueProps		`json:"valueProps"`
 	ExtProperties
 	Styles
 }
 
 type Wrapper struct{
 	Wrapper 	map[string]*interface{} `json:"wrapper"`
-}
-type ValueProps struct{
-	IsHtml	bool	`json:"isHtml"`
 }
