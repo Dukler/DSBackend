245a190bdaf667bfd7adc06907f2338089ddb6ac
dukler <iarwain@hotmail.com.ar>
2019-09-04 03:09:16 -0300

    chi instead of mux

--- go.mod
+++ go.mod
@@ -5,10 +5,10 @@ go 1.12
 require (
 	cloud.google.com/go v0.41.0
 	firebase.google.com/go v3.8.1+incompatible
-	github.com/gorilla/handlers v1.4.1
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/cors v1.0.0
 	github.com/gorilla/mux v1.7.3
 	github.com/lib/pq v1.1.1
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7
 	google.golang.org/api v0.7.0
-	google.golang.org/grpc v1.21.1
 )
--- main.go
+++ main.go
@@ -3,36 +3,47 @@ package main
 import (
 	"DSBackend/controllers"
 	"DSBackend/data"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 )
 
-var (
-	router *mux.Router
-)
+
 
 
 func main() {
-	router = mux.NewRouter()
+	router := *chi.NewRouter()
 
-	router.HandleFunc("/api/ui/update/{Screen}", controllers.UIEndpoint).Methods("GET")
-	router.HandleFunc("/api/do/{Action}", controllers.DoEndpoint).Methods("POST")
-	http.Handle("/", router)
+	c := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Requested-With",  "Access-Control-Allow-Headers"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	router.Use(c.Handler)
 
-	handler := handlers.CORS(
-		handlers.AllowedOrigins([]string{"*"}),
-		handlers.AllowedMethods([]string{"GET", "POST","OPTIONS"}),
-		handlers.AllowedHeaders([]string{"Content-Type", "X-Requested-With",  "Access-Control-Allow-Headers", "Authorization"}),
-	)(router)
+	router.Get("/api/ui/update/{Screen}", controllers.UIEndpoint)
+	router.Post("/api/do/{Action}", controllers.DoEndpoint)
+
+	http.Handle("/", &router)
+
+	//handler := handlers.CORS(
+	//	handlers.AllowedOrigins([]string{"*"}),
+	//	handlers.AllowedMethods([]string{"GET", "POST","OPTIONS"}),
+	//	handlers.AllowedHeaders([]string{"Content-Type", "X-Requested-With",  "Access-Control-Allow-Headers", "Authorization"}),
+	//)(router)
 
 	port :=  os.Getenv("PORT")
 
 	data.InitBucket("duckstackui")
 	//REST
-	log.Fatal(http.ListenAndServe(":"+ port, handler))
+	log.Fatal(http.ListenAndServe(":"+ port, &router))
 }
 
