fe7e8d63363c74dc2ceff452a188b9dea0acb6ea
dukler <iarwain@hotmail.com.ar>
2020-06-23 11:02:40 -0300

    firebase domain hardcode removed

--- data/data.go
+++ data/data.go
@@ -52,12 +52,12 @@ func GetApp(req *http.Request) *map[string][]interface{} {
 	domain := req.Header.Get("origin")
 	cleanURL(&domain)
 
-	if (domain == "duckstackui.firebaseapp.com"){
-		data["domain"] = "duckstack.com"
-	}else{
-		data["domain"] = domain
-	}
-	
+	// if (domain == "duckstackui.firebaseapp.com"){
+	// 	data["domain"] = "duckstack.com"
+	// }else{
+	// 	data["domain"] = domain
+	// }
+	data["domain"] = domain
 	data["token"] = req.Header.Get("Authorization")
 
 	jsonStr := readData(data)
